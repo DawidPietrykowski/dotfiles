65e16343ed29e95545b4b0e50ce268536154a1f8
Dawid Pietrykowski <dpietrykowski@antmicro.com>
2025-05-13 08:20:43 +0200

    Starship, alacritty, fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,12 +1,4 @@
 if status is-interactive
-    # argc-completions
-    # set -gx ARGC_COMPLETIONS_ROOT "$HOME/.config/fish/argc-completions"
-    # set -gx ARGC_COMPLETIONS_PATH "$HOME/.config/fish/argc-completions/completions"
-    # fish_add_path -m "$ARGC_COMPLETIONS_ROOT/bin"
-    # set argc_scripts (ls -1 "$HOME/.config/fish/argc-completions/completions" | sed -n 's/\.sh$//p')
-    #~/.config/fish/argc-completions/bin/argc --argc-completions fish $argc_scripts | source
-    # argc --argc-completions fish $argc_scripts | source
-
     starship init fish | source
 
     zoxide init fish | source
@@ -15,12 +7,13 @@ if status is-interactive
     uvx --generate-shell-completion fish | source
 
     fzf --fish | source
+
+    bind ctrl-e backward-kill-bigword
 end
 
 # Disable greeting
 set -g fish_greeting
 
-# abbr
 abbr -a psp --position anywhere pacman -Sy
 
 abbr -a l eza -l --color=always --group-directories-first
@@ -43,8 +36,6 @@ abbr -a stl --position anywhere systemctl
 
 abbr -a zl 'z $PWD'
 
-abbr -a h helix
-
 abbr -a hm history merge
 
 abbr -a uvv 'uv venv -q --allow-existing --seed; source .venv/bin/activate.fish'
@@ -55,8 +46,6 @@ abbr -a lg lazygit
 
 alias get_idf=". $HOME/storage/esp-idf/export.fish"
 alias get_matter="source $HOME/storage/esp-matter/export.sh"
-
-bind ctrl-e backward-kill-bigword
 
 # Locale
 set -gx LC_CTYPE en_US.UTF-8
@@ -73,12 +62,6 @@ fish_add_path ~/.cargo/bin
 fish_add_path ~/Android/Sdk/cmdline-tools/latest/bin/
 fish_add_path ~/.local/bin/
 
-# Add only for MacOS
-set -l os (uname)
-if test "$os" = Darwin
-    fish_add_path /opt/homebrew/bin
-end
-
 function fish_should_add_to_history
     # Ignore "set" commands if the variable being set contains "KEY" in the name
     if string match -qr '^set' -- $argv
@@ -91,15 +74,3 @@ function fish_should_add_to_history
     # If no conditions are met, add the command to history
     return 0
 end
-
-# fzf config
-# fzf_configure_bindings --directory=\cf
-
-# set fzf_preview_dir_cmd eza --all --color=always
-# set fzf_directory_opts --bind "ctrl-o:execute($EDITOR {} &> /dev/tty)"
-# set fzf_fd_opts --hidden
-
-# # fifc
-# set -U fifc_keybinding \cx
-
-# set -U fifc_fd_opts --hidden
